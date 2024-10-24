355a952df2b29498b35be437fb9f73107a95d38f
Sijad Khan <86185832+Sijadkhan1678@users.noreply.github.com>
2024-10-24 22:15:34 +0500

    Update server.js

--- server.js
+++ server.js
@@ -16,9 +16,12 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
     // Set Static Folder
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
-
+    //app.use(express.static('client/build'));
+    app.get('/', (req, res) => { 
+        app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+            
+        });
 
 const PORT = process.env.PORT || 5000;
 
