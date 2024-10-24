393cab0a08383f9cfe48b8493a4891b778e0a6ff
Sijad Khan <86185832+Sijadkhan1678@users.noreply.github.com>
2024-10-24 10:41:39 +0500

    Update server.js for production

--- server.js
+++ server.js
@@ -15,12 +15,10 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-// Serve static files on production server
-if(process.env.NODE_ENV === 'production') {
     // Set Static Folder
     app.use(express.static('client/build'));
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
-}
+
 
 const PORT = process.env.PORT || 5000;
 
